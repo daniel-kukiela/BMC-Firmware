e5aa9a45202e48fa3fb9611168191b979ab185f6
Daniel Kukieła <daniel@kukiela.pl>
2024-10-01 13:21:45 +0200

    Update config.sh

--- tp2bmc/board/tp2bmc/factory_overlay/upper/factory/config.sh
+++ tp2bmc/board/tp2bmc/factory_overlay/upper/factory/config.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
 PRODUCT_NAME="TuringPi2"
-PRODUCT_SERIAL_PREFIX=""
 # production month. format= 'YYmm'
 PRODUCTION_TIME="2408"
-
-RK1_USERNAME=ubuntu
-RK1_PASSWORD=Turingpi123
-
-# the amount allowed to wait for a module to boot (in secs)
-PING_TIMEOUT=60
-
-NODE1_IP="192.168.0.101"
-NODE2_IP="192.168.0.102"
-NODE3_IP="192.168.0.103"
-NODE4_IP="192.168.0.104"
-
-ASSERT_VERSION=4211
-ASSERT_MODEL_NAME="Turing Pi 2 (v2.5.1)"
 
 TPI_MAC_BASE="C4FF841"
