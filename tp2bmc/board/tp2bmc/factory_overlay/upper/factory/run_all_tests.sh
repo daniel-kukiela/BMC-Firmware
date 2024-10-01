de1cb8bdb40833be6f522359dbdce97df24d33e7
Daniel Kukieła <daniel@kukiela.pl>
2024-10-01 13:23:14 +0200

    Update run_all_tests.sh

--- tp2bmc/board/tp2bmc/factory_overlay/upper/factory/run_all_tests.sh
+++ tp2bmc/board/tp2bmc/factory_overlay/upper/factory/run_all_tests.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-source helpers.sh
-
-# Define color codes
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-GRAY='\033[0;90m'
-NC='\033[0m' # No Color
-
-# Define the directory to search
-TEST_DIR="/factory"
-NON_EXECUTABLE_FILES=()
-EXIT_CODE=0
-
 echo -e "\n\n${RED}TURING PI RECOVERY${NC}"
-
 
 source install_official_firmware.sh
 wait
